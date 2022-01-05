e6623ec3d989f77412ccbb3f94d63f7e857b7b75
Anmol Raj <course.anmolraj@gmail.com>
2022-01-05 09:36:25 +0530

    done with the login screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HomeScreen from './Screens/HomeScreen';
+import LoginScreen from './Screens/LoginScreen';
 
 function App() {
   return (
     <div className="App">
-      <HomeScreen />
+      {/* <HomeScreen /> */}
+      <LoginScreen />
     </div>
   );
 }
